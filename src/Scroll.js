41545d6edccd8eb17bc5b2c6b95d2ccd5b20a57a
Sean-Drew <SDrew989@gmail.com>
2021-03-25 12:52:42 -0400

    Add style to Scroll component
    Added style to Scroll component.
    Moved return items inside a div container.

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Scroll = (props) => {
-    return props.children
+    return (
+        <div style = {{ overflowY: 'scroll', borderTop: '3px solid black', padding: '10px', height: '750px' }}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Scroll
